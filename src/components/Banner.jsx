bdf9b585e12c292115bf5b741a78daf84fa5d829
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-30 12:55:39 +0600

    LinkedIN added in hero section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,6 +4,7 @@ import { TypeAnimation } from 'react-type-animation';
 import { FaFacebookF } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { Fade } from 'react-awesome-reveal';
+import { FaLinkedinIn } from 'react-icons/fa6';
 
 const Banner = () => {
     const [textcolor, setTextColor] = useState('white');
@@ -24,15 +25,15 @@ const Banner = () => {
 
                         <div className="flex-1 w-full relative place-items-center">
                             <div className="flex items-center justify-center">
-                                <div className="relative w-56 h-56 md:w-96 md:h-96">
+                                <div className="relative w-56 h-56 md:w-80 md:h-80">
                                     {/* Outer Circle */}
-                                    <div className="absolute inset-0 rounded-full bg-gradient-to-r from-blue-500  via-green-400 via-yellow-400 via-orange-500 to-pink-500  p-2 animate-spin-slow">
+                                    <div className="absolute inset-0 rounded-full bg-gradient-to-r from-blue-500  via-green-400 via-yellow-400 via-orange-500 to-pink-500 animate-spin-slow">
                                     </div>
                                     <div className="w-full h-full rounded-full bg-white flex items-center justify-center">
                                         {/* Profile Image */}
-                                        <div className="relative w-52 h-52 md:w-80 md:h-80 bg-gradient-to-r from-blue-400 to-purple-500 overflow-hidden rounded-full ">
+                                        <div className="relative w-52 h-52 md:w-72 md:h-72 bg-gradient-to-r from-blue-400 to-purple-500 overflow-hidden rounded-full ">
                                             <img
-                                                src="https://i.ibb.co/vDG0t8q/bg-image.png"
+                                                src="https://i.ibb.co.com/vDG0t8q/bg-image.png"
                                                 alt="Profile"
                                                 className="w-full h-full object-cover rounded-full"
                                             />
@@ -74,11 +75,13 @@ const Banner = () => {
                             </p>
                             <p className='text-slate-200 text-xl'>   I specialize in creating innovative web solutions with a focus on design, functionality, and user satisfaction. Bringing ideas to life with clean code and creative designs.</p>
                             <div className='space-y-5 mt-4'>
-                                <div className='flex items-center mb-6 justify-start gap-5'>
+                                <div className='flex items-center mb-6 justify-start gap-2'>
                                     <a href='https://www.facebook.com/majidul1230' target="_blank"
-                                        rel="noopener noreferrer" className='border p-2 rounded-full bg-black bg-gradient-to-r hover:from-purple-600 hover:to-blue-500'>  <FaFacebookF size={24} /></a>
+                                        rel="noopener noreferrer" className='border p-2 rounded-full bg-black hover:bg-blue-500'>  <FaFacebookF className='md:text-2xl ' /></a>
                                     <a href='https://github.com/pro-majidul' target="_blank"
-                                        rel="noopener noreferrer" className='border p-2 rounded-full bg-black bg-gradient-to-r hover:from-purple-600 hover:to-blue-500'>  <FaGithub size={24} /></a>
+                                        rel="noopener noreferrer" className='border p-2 rounded-full bg-black bg-gradient-to-r hover:from-purple-600 hover:to-blue-500'>  <FaGithub className='md:text-2xl' /></a>
+                                    <a href='https://www.linkedin.com/in/majidul-islam7' target="_blank"
+                                        rel="noopener noreferrer" className='border p-2 rounded-full bg-black bg-gradient-to-r hover:from-purple-600 hover:to-blue-500'>  <FaLinkedinIn className='md:text-2xl' /></a>
                                 </div>
                                 <a download
                                     href='https://drive.google.com/uc?export=download&id=1xULRceWwX4n5yOTUT-zLvqYJx4hgQeH3' className="px-6 py-3 text-white font-semibold rounded-lg bg-gradient-to-r from-blue-500 to-purple-600 hover:from-purple-600 hover:to-blue-500 transition-all duration-300 hover:text-orange-400 cursor-pointer">DownLoad Resume</a>
